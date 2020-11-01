ace46af15926b6b2071661b41a996c8fa4d96223
Vikky <41500162+vikky-noelle@users.noreply.github.com>
2020-11-01 17:58:49 +0530

    Update index.ts
    Added the dp approach and incorporated the cache mechanism.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,44 @@
-export function fibonacci(n: number): number | string {
-    // golden number
-    const phi = (1 - Math.sqrt(5)) / 2;
-    const Phi = (1 + Math.sqrt(5)) / 2;
-    if (n > 0)
-        return Math.round((Math.pow(Phi, n - 1) - Math.pow(phi, n - 1)) / Math.sqrt(5));
-    else
-        return "invalid input";
+import { Cache } from './cache';
+
+// object of cache class
+let cache = new Cache();
+
+export function math_fibonacci(n: number): number | string {
+    if (cache.check(n)) {
+        return cache.get(n);
+    }
+    else {
+        // golden number
+        const phi = (1 - Math.sqrt(5)) / 2;
+        const Phi = (1 + Math.sqrt(5)) / 2;
+        if (n > 0) {
+            var result = Math.round((Math.pow(Phi, n - 1) - Math.pow(phi, n - 1)) / Math.sqrt(5));
+            cache.set(n, result);
+            return result;
+        }
+        else
+            return "invalid input"
+    }
 }
 
+function reccursion_fn_to_get_fibonacci(n: number): number {
+    if (cache.check(n)) {
+        return cache.get(n);
+    }
+    else {
+        var result = reccursion_fn_to_get_fibonacci(n - 1) + reccursion_fn_to_get_fibonacci(n - 2);
+        cache.set(n, result);
+        return result;
+    }
+}
+export function dp_fibonacci(n: number): number | string {
+    if (n <= 0) {
+        return "invalid input";
+    }
+    else if (cache.check(n)) {
+        return cache.get(n);
+    }
+    else {
+        return reccursion_fn_to_get_fibonacci(n);
+    }
+}
