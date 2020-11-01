6b075f55e07877c28b4424b263412820d7051298
Vikky <41500162+vikky-noelle@users.noreply.github.com>
2020-11-01 17:59:44 +0530

    Update test.ts
    tests updated for dp approach.

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,20 @@
-import { fibonacci } from "../src/index";
+import { math_fibonacci, dp_fibonacci } from "../src/index";
 
-test('My fibonacci program', () => {
-    expect(fibonacci(3)).toBe(1);
-    expect(fibonacci(5)).toBe(3);
-    expect(fibonacci(11)).toBe(55);
-    expect(fibonacci(0)).toBe("invalid input");
+test('My math_fibonacci program', () => {
+    expect(math_fibonacci(1)).toBe(0);
+    expect(math_fibonacci(2)).toBe(1);
+    expect(math_fibonacci(3)).toBe(1);
+    expect(math_fibonacci(3)).toBe(1);
+    expect(math_fibonacci(5)).toBe(3);
+    expect(math_fibonacci(11)).toBe(55);
+    expect(math_fibonacci(0)).toBe("invalid input");
+});
+
+test('My dp_fibonacci program', () => {
+    expect(dp_fibonacci(11)).toBe(55);
+    expect(dp_fibonacci(1)).toBe(0);
+    expect(dp_fibonacci(2)).toBe(1);
+    expect(dp_fibonacci(3)).toBe(1);
+    expect(dp_fibonacci(5)).toBe(3);
+    expect(dp_fibonacci(0)).toBe("invalid input");
 });
