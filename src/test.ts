5086212df7b27cbb6e73ca436b6b0191de99d27c
Vikky <41500162+vikky-noelle@users.noreply.github.com>
2020-11-02 09:37:34 +0530

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -11,11 +11,11 @@ test('My math_fibonacci program', () => {
 });
 
 test('My dp_fibonacci program', () => {
-    expect(math_fibonacci(0)).toBe(0);
-    expect(math_fibonacci(1)).toBe(1);
-    expect(math_fibonacci(2)).toBe(1);
-    expect(math_fibonacci(3)).toBe(2);
-    expect(math_fibonacci(5)).toBe(5);
-    expect(math_fibonacci(10)).toBe(55);
-    expect(math_fibonacci(-1)).toBe("invalid input");
+    expect(dp_fibonacci(0)).toBe(0);
+    expect(dp_fibonacci(1)).toBe(1);
+    expect(dp_fibonacci(2)).toBe(1);
+    expect(dp_fibonacci(3)).toBe(2);
+    expect(dp_fibonacci(5)).toBe(5);
+    expect(dp_fibonacci(10)).toBe(55);
+    expect(dp_fibonacci(-1)).toBe("invalid input");
 });
