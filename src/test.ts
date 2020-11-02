39ff1965e9bcd82a172d4c2692b0c2f04dbf625e
Vikky <41500162+vikky-noelle@users.noreply.github.com>
2020-11-02 09:19:57 +0530

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -1,20 +1,21 @@
 import { math_fibonacci, dp_fibonacci } from "../src/index";
 
 test('My math_fibonacci program', () => {
-    expect(math_fibonacci(1)).toBe(0);
+    expect(math_fibonacci(0)).toBe(0);
+    expect(math_fibonacci(1)).toBe(1);
     expect(math_fibonacci(2)).toBe(1);
-    expect(math_fibonacci(3)).toBe(1);
-    expect(math_fibonacci(3)).toBe(1);
-    expect(math_fibonacci(5)).toBe(3);
-    expect(math_fibonacci(11)).toBe(55);
-    expect(math_fibonacci(0)).toBe("invalid input");
+    expect(math_fibonacci(3)).toBe(2);
+    expect(math_fibonacci(5)).toBe(5);
+    expect(math_fibonacci(10)).toBe(55);
+    expect(math_fibonacci(-1)).toBe("invalid input");
 });
 
 test('My dp_fibonacci program', () => {
-    expect(dp_fibonacci(11)).toBe(55);
-    expect(dp_fibonacci(1)).toBe(0);
-    expect(dp_fibonacci(2)).toBe(1);
-    expect(dp_fibonacci(3)).toBe(1);
-    expect(dp_fibonacci(5)).toBe(3);
-    expect(dp_fibonacci(0)).toBe("invalid input");
+    expect(math_fibonacci(0)).toBe(0);
+    expect(math_fibonacci(1)).toBe(1);
+    expect(math_fibonacci(2)).toBe(1);
+    expect(math_fibonacci(3)).toBe(2);
+    expect(math_fibonacci(5)).toBe(5);
+    expect(math_fibonacci(10)).toBe(55);
+    expect(math_fibonacci(-1)).toBe("invalid input");
 });
